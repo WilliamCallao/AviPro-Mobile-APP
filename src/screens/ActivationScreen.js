5e3d7a1d3095888d867f171669a421a4e7c93e89
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-06-19 20:04:39 -0400

    Generación APK

--- src/screens/ActivationScreen.js
+++ src/screens/ActivationScreen.js
@@ -54,10 +54,12 @@ const ActivationScreen = () => {
 
   const handleSaludo = async () => {
     try {
-      const response = await axios.get(`${BASE_URL}/api/saludo`);
-      Alert.alert("Saludo", response.data.message);
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+      console.log("response", response.data);
+      Alert.alert("Saludo", "Recibido: " + response.data.title);
     } catch (e) {
       Alert.alert("Error", "No se pudo obtener el saludo.");
+      console.error('Error fetching public API:', e);
     }
   };
 
@@ -97,7 +99,7 @@ const ActivationScreen = () => {
                 ) : (
                   <>
                     <SimpleButton text="Activar" onPress={handleActivate} width={styles.button.width} />
-                    <SimpleButton text="Obtener Saludo" onPress={handleSaludo} width={styles.button.width} />
+                    {/* <SimpleButton text="Obtener Saludo" onPress={handleSaludo} width={styles.button.width} /> */}
                   </>
                 )}
                 <StyledText style={styles.softText}>Si no tiene un código de activación, por favor contacte a nuestro equipo de ventas para adquirir una licencia.</StyledText>
