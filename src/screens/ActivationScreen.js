d13059c9b68380dbd01b67b002ce45a8a488757c
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-06-21 13:21:31 -0400

    Format

--- src/screens/ActivationScreen.js
+++ src/screens/ActivationScreen.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
-import { Image, StyleSheet, View, TextInput, Dimensions, Alert, KeyboardAvoidingView, Platform, ScrollView, ActivityIndicator } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  TextInput,
+  Dimensions,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import userStore from "../stores/userStore";
 import StyledText from "../utils/StyledText";
 import SimpleButton from "../utils/SimpleButton";
-import axios from 'axios';
+import axios from "axios";
 import { BASE_URL } from "../../config";
 import { theme } from "../assets/Theme";
 
-const windowWidth = Dimensions.get('window').width;
+const windowWidth = Dimensions.get("window").width;
 const aspectRatio = 5285 / 5315;
 
 const ActivationScreen = () => {
@@ -18,8 +29,8 @@ const ActivationScreen = () => {
   const [loading, setLoading] = useState(false);
   const [activationSuccess, setActivationSuccess] = useState(false);
 
-  const { setEmpresa } = userStore(state => ({
-    setEmpresa: state.setEmpresa
+  const { setEmpresa } = userStore((state) => ({
+    setEmpresa: state.setEmpresa,
   }));
 
   const handleActivate = async () => {
@@ -31,18 +42,22 @@ const ActivationScreen = () => {
     setLoading(true);
     setMessage("");
 
-    const minLoadingTime = new Promise(resolve => setTimeout(resolve, 2000));
+    const minLoadingTime = new Promise((resolve) => setTimeout(resolve, 2000));
 
     try {
       const [response] = await Promise.all([
-        axios.put(`${BASE_URL}/api/mobile/dispositivos/estado/usado/${activationCode}`),
-        minLoadingTime
+        axios.put(
+          `${BASE_URL}/api/mobile/dispositivos/estado/usado/${activationCode}`
+        ),
+        minLoadingTime,
       ]);
 
       const { message, dispositivo } = response.data;
 
       if (message === "El código fue activado correctamente") {
-        await axios.put(`${BASE_URL}/api/mobile/dispositivos/ultimo_uso/${activationCode}`);
+        await axios.put(
+          `${BASE_URL}/api/mobile/dispositivos/ultimo_uso/${activationCode}`
+        );
         setMessage("El código fue activado correctamente.");
         setActivationSuccess(true);
         setEmpresa("EmpresaAsignada");
@@ -62,12 +77,12 @@ const ActivationScreen = () => {
 
   const handleSaludo = async () => {
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+      const response = await axios.get(`${BASE_URL}/api/saludo`);
       console.log("response", response.data);
-      Alert.alert("Saludo", "Recibido: " + response.data.title);
+      Alert.alert("Saludo", "Recibido: " + response.data);
     } catch (e) {
       Alert.alert("Error", "No se pudo obtener el saludo.");
-      console.error('Error fetching public API:', e);
+      console.error("Error fetching public API:", e);
     }
   };
 
@@ -85,38 +100,73 @@ const ActivationScreen = () => {
       >
         <ScrollView contentContainerStyle={styles.scrollViewContent}>
           <View style={styles.containerImgs}>
-            <Image source={require('../assets/formas.png')} style={{ width: windowWidth * 0.75, height: windowWidth * 0.75 * aspectRatio }} />
+            <Image
+              source={require("../assets/formas.png")}
+              style={{
+                width: windowWidth * 0.75,
+                height: windowWidth * 0.75 * aspectRatio,
+              }}
+            />
           </View>
           <View style={styles.innerContainer}>
             <StyledText style={styles.title}>Avi Pro Mobile</StyledText>
             {!activationSuccess ? (
               <>
-                <StyledText style={styles.subtitle}>Ingrese su código de activación</StyledText>
+                <StyledText style={styles.subtitle}>
+                  Ingrese su código de activación
+                </StyledText>
                 <TextInput
                   placeholder="XXXX-XXXX-XXXX-XXXX"
                   style={styles.label}
-                  onChange={(code) => { setActivationCode(code.nativeEvent.text) }}
+                  onChange={(code) => {
+                    setActivationCode(code.nativeEvent.text);
+                  }}
                   value={activationCode}
                   keyboardType="default"
                   autoCapitalize="characters"
                   autoCorrect={false}
                 />
-                {message && <StyledText style={styles.errorFormat}>{message}</StyledText>}
+                {message && (
+                  <StyledText style={styles.errorFormat}>{message}</StyledText>
+                )}
                 {loading ? (
-                  <ActivityIndicator size="large" color={theme.colors.primary} />
+                  <ActivityIndicator
+                    size="large"
+                    color={theme.colors.primary}
+                  />
                 ) : (
                   <>
-                    <SimpleButton text="Activar" onPress={handleActivate} width={styles.button.width} />
-                    {/* <SimpleButton text="Obtener Saludo" onPress={handleSaludo} width={styles.button.width} /> */}
+                    <SimpleButton
+                      text="Activar"
+                      onPress={handleActivate}
+                      width={styles.button.width}
+                    />
+                    {/* <SimpleButton
+                      text="Obtener Saludo"
+                      onPress={handleSaludo}
+                      width={styles.button.width}
+                    /> */}
                   </>
                 )}
-                <StyledText style={styles.softText}>Si no tiene un código de activación, por favor contacte a nuestro equipo de ventas para adquirir una licencia.</StyledText>
+                <StyledText style={styles.softText}>
+                  Si no tiene un código de activación, por favor contacte a
+                  nuestro equipo de ventas para adquirir una licencia.
+                </StyledText>
               </>
             ) : (
               <>
                 <StyledText style={styles.successFormat}>{message}</StyledText>
-                <StyledText style={styles.softText}>Al continuar, acepta todos los términos, condiciones y políticas de privacidad.</StyledText>
-                {activationSuccess && <SimpleButton text="Continuar" onPress={handleContinue} width={styles.button.width} />}
+                <StyledText style={styles.softText}>
+                  Al continuar, acepta todos los términos, condiciones y
+                  políticas de privacidad.
+                </StyledText>
+                {activationSuccess && (
+                  <SimpleButton
+                    text="Continuar"
+                    onPress={handleContinue}
+                    width={styles.button.width}
+                  />
+                )}
               </>
             )}
           </View>
@@ -129,7 +179,7 @@ const ActivationScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#9DBBE2',
+    backgroundColor: "#9DBBE2",
   },
   keyboardAvoidingView: {
     flex: 1,
@@ -137,63 +187,63 @@ const styles = StyleSheet.create({
   },
   scrollViewContent: {
     flexGrow: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   containerImgs: {
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    alignItems: "center",
+    justifyContent: "flex-start",
     paddingTop: 80,
     paddingBottom: 10,
   },
   innerContainer: {
     flex: 1,
     padding: 20,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginBottom: 20,
   },
   subtitle: {
     fontSize: 18,
     marginBottom: 10,
-    textAlign: 'center',
+    textAlign: "center",
   },
   label: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     marginVertical: 10,
     padding: 10,
     borderRadius: 10,
     fontSize: 15,
-    textAlign: 'center',
+    textAlign: "center",
     letterSpacing: 4,
   },
   softText: {
     color: theme.colors.gray,
     fontSize: 15,
     marginVertical: 18,
-    textAlign: 'center',
+    textAlign: "center",
   },
   errorFormat: {
-    color: 'red',
+    color: "red",
     fontSize: 15,
     marginTop: -8,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
   },
   successFormat: {
-    color: 'green',
+    color: "green",
     fontSize: 20,
     marginTop: 20,
-    textAlign: 'center',
+    textAlign: "center",
     marginHorizontal: 40,
   },
   button: {
     backgroundColor: theme.colors.tertiary,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 10,
     padding: 10,
     marginVertical: 15,
@@ -201,7 +251,7 @@ const styles = StyleSheet.create({
   continueButton: {
     color: theme.colors.primary,
     fontSize: 19,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
