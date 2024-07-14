f5a9d6346ee65dc5689e57ba1c0afbe2a65af0ff
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-07-14 08:20:43 -0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default{
   expo: {
     name: "avipro-app",
     slug: "avipro-app",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
